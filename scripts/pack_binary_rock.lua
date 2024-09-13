d32a158784e6425ad3de0d5d8242452675dd1da4
Marc Jakobi <marc.jakobi@tiko.energy>
2024-09-13 17:50:56 +0200

    revert(windows): debug print

--- scripts/pack_binary_rock.lua
+++ scripts/pack_binary_rock.lua
@@ -9,7 +9,6 @@ local arch = arg[2]
 
 local luarocks_cmd
 if vim.uv.os_uname().sysname:lower():find("windows") then
-  vim.print("luarocks.bat executable: " .. tostring(vim.fn.executable("luarocks.bat")))
   -- HACK: Hardcode luarocks path, as it can't seem to be found by vim.system.
   luarocks_cmd = vim.print(assert(vim.fn.glob("C:/Users/runneradmin/scoop/apps/luarocks/current/luarocks"), "luarocks executable not found"))
 else
